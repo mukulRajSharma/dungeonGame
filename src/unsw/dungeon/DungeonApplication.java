abbb74f5488bf8a6ab7685346df5717094ec8267
= <z5220980@unsw.edu.au>
2020-07-19 18:11:49 +1000

    filename in loader changed to portals

--- src/unsw/dungeon/DungeonApplication.java
+++ src/unsw/dungeon/DungeonApplication.java
@@ -17,7 +17,7 @@ public class DungeonApplication extends Application {
         window = primaryStage;
 
         window.setTitle("Dungeon");
-        FXMLLoader loader = levelLoader("boulders.json");
+        FXMLLoader loader = levelLoader("portals.json");
         Parent root = loader.load();
         Scene scene = new Scene(root);
         root.requestFocus();
