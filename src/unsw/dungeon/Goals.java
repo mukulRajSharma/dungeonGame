4ae8e8548f65205b2d0769460b53def3d865bfab
= <z5220980@unsw.edu.au>
2020-07-19 14:53:53 +1000

    added missing goals class

---
+++ src/unsw/dungeon/Goals.java
@@ -0,0 +1,93 @@
+package unsw.dungeon;
+
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+
+
+
+public class Goals {
+    private SimpleBooleanProperty goalsMet;
+    private ArrayList<String> winConditions;
+    private Dungeon dungeon;
+
+    public Goals(Dungeon d){
+        goalsMet = new SimpleBooleanProperty();
+        goalsMet.set(false);
+        winConditions = new ArrayList<String>();
+        this.dungeon = d;
+    }
+
+    public BooleanProperty complete(){
+        return goalsMet;
+    }
+
+    public void setGoalsMet(boolean b){
+        goalsMet.set(b);
+    }
+
+
+    // Stores goals like {AND, enemies, OR, exit, treasure} 
+    public void addGoals(JSONObject goals){
+        String goal = goals.getString("goal");
+        winConditions.add(goal);
+        if(goal.equals("AND") || goal.equals("OR")){
+            JSONArray subgoals = goals.getJSONArray("subgoals");
+            for(int i = 0; i < subgoals.length(); i++){
+                this.addGoals(subgoals.getJSONObject(i));
+            }
+        }
+        System.out.println(winConditions.toString());
+    }
+
+    public boolean winGame(){
+        boolean win = hasWon(0);
+        setGoalsMet(win);
+        return win;
+    }
+
+    private boolean hasWon(int curr){
+        if(winConditions.get(curr).equals("AND")){
+            if(this.hasWon(curr+1) && this.hasWon(curr+2)){
+                return true;
+            } else {
+                return false;
+            }
+        } else if(winConditions.get(curr).equals("OR")){
+            if(this.hasWon(curr+1) || this.hasWon(curr+2)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        boolean endCondition = false;
+        switch(winConditions.get(curr)){
+            case "treasure":
+                endCondition = dungeon.treasureEndCondition();
+                break;
+            case "exit":
+                endCondition = dungeon.exitEndCondition();
+                break;
+            case "boulder":
+                endCondition = dungeon.boulderEndCondition();
+                break;
+            case "enemies":
+                endCondition = dungeon.enemyEndCondition();
+                break;
+        }
+        return endCondition;
+    }
+
+
+    @Override
+    public String toString(){
+        return winConditions.toString();
+    }
+
+    
+}
